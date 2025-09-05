429d2deeb39a03a8232a1fd5ca965cab1a2e2f0b
donggun <hugh3232@naver.com>
2025-09-05 16:25:04 +0900

    loan엔티티 수정

--- src/domain/loan/entity/Loan.entity.ts
+++ src/domain/loan/entity/Loan.entity.ts
@@ -1,19 +1,24 @@
-// src/domain/loan/entity/Loan.entity.ts
-import { Entity, PrimaryGeneratedColumn, ManyToOne, Column, CreateDateColumn, UpdateDateColumn } from 'typeorm';
+import { Entity, PrimaryGeneratedColumn, ManyToOne, Column, CreateDateColumn, JoinColumn } from 'typeorm';
 import {UserEntity} from "../../users/entity/Users.entity";
 import {BookEntity} from "../../books/entity/Books.entity";
-
 
 @Entity('loans')
 export class LoanEntity {
     @PrimaryGeneratedColumn('uuid')
     id: string;
 
-    //하나의 대출(Loan)은 **하나의 사용자(User)**에 속함 + 하나의 사용자는 여러 대출(Loan[])을 가질 수 있음
-    @ManyToOne(() => UserEntity, (user) => user.loans, { eager: true })
+    @ManyToOne(() => UserEntity, (user) => user.loans, {
+        eager: true,
+        onDelete: 'CASCADE'  // 사용자 삭제 시 대출 기록도 삭제
+    })
+    @JoinColumn({ name: 'user_id' }) // FK 컬럼명 지정
     user: UserEntity;
-    //하나의 책은 여러 대출을 가질수 있음
-    @ManyToOne(() => BookEntity, (book) => book.loans, { eager: true })
+
+    @ManyToOne(() => BookEntity, (book) => book.loans, {
+        eager: true,
+        onDelete: 'CASCADE'  // 책 삭제 시 대출 기록도 삭제
+    })
+    @JoinColumn({ name: 'book_id' }) // FK 컬럼명 지정
     book: BookEntity;
 
     @CreateDateColumn()
